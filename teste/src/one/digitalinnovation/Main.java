2917389f1afb4f1d599aee13f5aea71a1acc6500
AlbertoAguiar2010 <albertoaguiar2010@gmail.com>
2021-07-16 18:45:44 -0300

    test

---
+++ teste/src/one/digitalinnovation/Main.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+    }
+}
---
+++ teste/src/one/digitalinnovation/teste.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation;
+
+public class teste {
+    public static void main(String[] args) {
+       var nome = "hello word";
+        int[] list1 = {87, 68, -52, 5, 49, -83, -45, -12, -64};
+
+
+
+            }
+}
+
