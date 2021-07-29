99e3ae8e99cd96ad7067e0a7d8b7de40213d82b0
AlbertoAguiar2010 <albertoaguiar2010@gmail.com>
2021-07-29 11:36:39 -0300

    treino dotnet DIO

--- dotnetdio/Program.cs
+++ dotnetdio/Program.cs
@@ -7,6 +7,7 @@ namespace dotnetdio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello DIO codes!!!!");
         }
     }
 }
