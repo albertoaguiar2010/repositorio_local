bbdb7ca52149df44c720063d3bcda19a3a53c91d
AlbertoAguiar2010 <albertoaguiar2010@gmail.com>
2021-08-11 16:07:47 -0300

    fim aula

--- projetos_DIO_Java/src/main/java/com/projeto/Fila/src/main/java/one/digitalinnovation/No.java
+++ projetos_DIO_Java/src/main/java/com/projeto/Fila/src/main/java/one/digitalinnovation/No.java
@@ -1,2 +1,36 @@
-package one.digitalinnovation;public class No {
+package one.digitalinnovation;
+public class No <T> {
+    private T  object;
+
+    private No <T> refNo = null;
+
+    public No() {
+    }
+
+    public No(T object) {
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dado=" + object +
+                '}';
+    }
 }
